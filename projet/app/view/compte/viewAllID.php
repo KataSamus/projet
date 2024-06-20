4afbe84802a8bc93a0a9f18da4a57c6e3bf28f37
combe <combe@omen-noe>
2024-06-20 13:25:35 +0200

    Modif d'une vue

--- projet/app/view/compte/viewAllID.php
+++ projet/app/view/compte/viewAllID.php
@@ -12,27 +12,29 @@ require ($root . '/app/view/fragment/fragmentPatrimoineHeader.html');
       include $root . '/app/view/fragment/fragmentPatrimoineJumbotron.html';
       ?>
 
+    <?php if ($results) { ?>
     <table class = "table table-striped table-bordered">
       <thead>
         <tr>
-          <th scope = "col">banque</th>
-          <th scope = "col">label</th>
-          <th scope = "col">montant</th>
+          <th scope = "col">Banque</th>
+          <th scope = "col">Label</th>
+          <th scope = "col">Montant</th>
         </tr>
       </thead>
       <tbody>
           <?php
-          // La liste des producteurs est dans une variable $results             
-          foreach ($results as $element) {
-            printf("<tr><td>%s</td><td>%s</td><td>%d</td></tr>", $element['banque'], $element['label'], $element['montant']);
-          }
-          ?>
+            foreach ($results as $element) {
+             printf("<tr><td>%s</td><td>%s</td><td>%d</td></tr>", $element['banque'], 
+               $element['label'], $element['montant']);
+            }?>
       </tbody>
     </table>
+    <?php } else {
+        echo("<h3>Vous ne possédez pas de compte<h3/>");
+    }?>
   </div>
   <?php include $root . '/app/view/fragment/fragmentPatrimoineFooter.html'; ?>
 
   <!-- ----- fin viewAllID -->
-  
   
   
